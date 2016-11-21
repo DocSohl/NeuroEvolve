6943f8af83466eb08008e1a99602cdca0e3ee7cd
Ian Sohl <isohl@apple.com>
2016-11-21 10:44:12 -0800

    Aero work from some time ago

--- Aero/Quad.py
+++ Aero/Quad.py
@@ -1,31 +1,92 @@
 import threading
 from collections import namedtuple
 import pygame
+import numpy as np
+import quaternion
 
-Params = namedtuple("Params", "x y z rx ry rz")
+Props = namedtuple("Props", "a b c d")
+
+
+# Propeller configuration
+# A     B    ^ z
+#  \   /     |
+#   |-|      y--> x
+#  /   \
+# D     C
+# Clockwise: A, C
+# Counter: B, C
 
 def runQuad(q):
     q.run()
 
+
 class Quad:
     def __init__(self):
         self._lock = threading.Lock()
-        self._params = Params(0,0,0,0,0,0)
+        self._position = np.array([0, 0, 0], dtype='float32').T
+        self._orientation = np.quaternion(1, 0, 0, 0)
+        self._velocity = np.array([0, 0, 0], dtype='float32').T
+        self._rotational_velocity = np.array([0, 0, 0], dtype='float32')
+        self._acceleration = np.array([0, 0, 0], dtype='float32').T
+        self._rotational_acceleration = np.array([0, 0, 0], dtype='float32')
+        self._props = Props(2.0, 3.0, 2.0, 3.0)
         self._running = True
         self._clock = pygame.time.Clock()
+        self._dt = 0.2
 
-    def get_params(self):
+    @property
+    def params(self):
         with self._lock:
-            return self._params
+            return self._position, self._orientation
 
-    def set_params(self, x, y, z, rx, ry, rz):
+    @property
+    def props(self):
         with self._lock:
-            self._params = Params(x,y,z,rx,ry,rz)
+            return self._props
+
+    @props.setter
+    def props(self, p):
+        with self._lock:
+            self._props = p
+
+    _accels = {
+        0: np.array([ 1,  1,  1]).T,
+        1: np.array([ 1, -1, -1]).T,
+        2: np.array([-1,  1, -1]).T,
+        3: np.array([-1, -1,  1]).T
+    }
+
+    def _apply_acceleration(self, orientation, props):
+        thrust = 0
+        self._acceleration = np.array([0, 0, 0], dtype='float32').T
+        accel_rotation = np.array([0,0,0], dtype='float32').T
+        for i, p in enumerate(props):
+            response = Quad._accels[i]
+            accel_rotation += p * response
+            thrust += p
+        rotate = quaternion.as_rotation_matrix(orientation)
+        thrust_vector = rotate.dot(np.array([0, 1, 0], dtype='float32').T)
+        self._acceleration += thrust * thrust_vector
+        gravity_vector = np.array([0, -10, 0], dtype='float32').T
+        self._acceleration += gravity_vector
+        # self._rotational_acceleration = quaternion.from_euler_angles(accel_rotation[0], accel_rotation[1], accel_rotation[2])
+
+    def _move(self, pos):
+        self._velocity += self._acceleration * self._dt
+        self._rotational_velocity += self._rotational_acceleration * self._dt
+        with self._lock:
+            self._position += self._velocity * self._dt
+            self._orientation += self._rotational_velocity * self._dt
+
+    def _get_setup(self):
+        with self._lock:
+            return self._position, self._orientation, self._props
 
     def run(self):
         while self._running:
-            p = self._params
-            self.set_params(p.x, p.y, p.z, p.rx+0, p.ry+1, p.rz+0)
+            pos, orientation, props = self._get_setup()
+            self._apply_acceleration(orientation, props)
+            self._move(pos)
             self._clock.tick(50)
 
     def quit(self):
--- Aero/Simulate.py
+++ Aero/Simulate.py
@@ -2,8 +2,9 @@ import pygame
 import sys
 from operator import itemgetter
 import threading
+import numpy as np
+import quaternion
 
-from Point3D import Point3D
 from Quad import Quad, runQuad
 
 class Simulation:
@@ -11,20 +12,21 @@ class Simulation:
         pygame.init()
         self.screen = pygame.display.set_mode((width, height))
         self.clock = pygame.time.Clock()
-        self.vertices = [
-            Point3D(-1, 1, -1),
-            Point3D(1, 1, -1),
-            Point3D(1, -1, -1),
-            Point3D(-1, -1, -1),
-            Point3D(-1, 1, 1),
-            Point3D(1, 1, 1),
-            Point3D(1, -1, 1),
-            Point3D(-1, -1, 1)
-        ]
+        self.vertices = np.array([
+            [-1, 1, -1],
+            [1, 1, -1],
+            [1, -1, -1],
+            [-1, -1, -1],
+            [-1, 1, 1],
+            [1, 1, 1],
+            [1, -1, 1],
+            [-1, -1, 1]
+        ], dtype='float32')
         self.faces = [(0, 1, 2, 3), (1, 5, 6, 2), (5, 4, 7, 6), (4, 0, 3, 7), (0, 4, 5, 1), (3, 2, 6, 7)]
         self.colors = [(255, 0, 255), (255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 255, 255), (255, 255, 0)]
         self.angle = 0
         self.quad = Quad()
+
 
     def run(self):
         t = threading.Thread(target=runQuad, args=(self.quad,))
@@ -39,26 +41,26 @@ class Simulation:
             self.clock.tick(50)
             self.screen.fill((0, 32, 0))
 
-            t = []
-            params = self.quad.get_params()
+            position, orientation = self.quad.params
+            rotation = quaternion.as_rotation_matrix(orientation)
+            t = self.vertices.dot(rotation) + position
 
-            for v in self.vertices:
-                r = v.rotateX(params.rx).rotateY(params.ry).rotateZ(params.rz)
-                p = r.project(self.screen.get_width(), self.screen.get_height(), 256, 4)
-                t.append(p)
+            factors = 256 / (4 + t[:,2]) # fov / viewer_distance + z
+            t[:, 0] =  t[:, 0] * factors + self.screen.get_width() / 2
+            t[:, 1] = -t[:, 1] * factors + self.screen.get_height() / 2
 
             avg_z = []
             for i, f in enumerate(self.faces):
-                z = (t[f[0]].z + t[f[1]].z + t[f[2]].z + t[f[3]].z) / 4.0
+                z = (t[f[0],2] + t[f[1],2] + t[f[2],2] + t[f[3],2]) / 4.0
                 avg_z.append([i, z])
 
             for tmp in sorted(avg_z, key=itemgetter(1), reverse=True):
                 face_index = tmp[0]
                 f = self.faces[face_index]
-                pointlist = [(t[f[0]].x, t[f[0]].y), (t[f[1]].x, t[f[1]].y),
-                             (t[f[1]].x, t[f[1]].y), (t[f[2]].x, t[f[2]].y),
-                             (t[f[2]].x, t[f[2]].y), (t[f[3]].x, t[f[3]].y),
-                             (t[f[3]].x, t[f[3]].y), (t[f[0]].x, t[f[0]].y)]
+                pointlist = [(t[f[0],0], t[f[0],1]), (t[f[1],0], t[f[1],1]),
+                             (t[f[1],0], t[f[1],1]), (t[f[2],0], t[f[2],1]),
+                             (t[f[2],0], t[f[2],1]), (t[f[3],0], t[f[3],1]),
+                             (t[f[3],0], t[f[3],1]), (t[f[0],0], t[f[0],1])]
                 pygame.draw.polygon(self.screen, self.colors[face_index], pointlist)
             self.angle += 1
 
